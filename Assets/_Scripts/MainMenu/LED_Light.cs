6e2ffb5194008b8cc5c33f5b4f2227ead8189f4d
Sepu <astrosepustudio@gmail.com>
2026-03-10 19:43:22 -0300

    Led Flicker
    Added light flickering in the main menu scene

---
+++ Assets/_Scripts/MainMenu/LED_Light.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class LED_Light : MonoBehaviour
+{
+    [SerializeField] Light ledLight;
+    [SerializeField] Renderer lightRenderer;
+
+    Material lMat;
+
+    public float Intensity
+    {
+        get => ledLight.intensity;
+        set
+        {
+            ledLight.intensity = value;
+            //lMat.SetColor("_EmissionColor", Color.white * value);
+        }
+    }
+
+    private void Awake()
+    {
+        lMat = new Material(lightRenderer.material);
+        lightRenderer.material = lMat;
+    }
+
+    public void SwitchLight(bool enable)
+    {
+        ledLight.enabled = enable;
+
+        if (enable)
+            lMat.EnableKeyword("_EMISSION");
+        else
+            lMat.DisableKeyword("_EMISSION");
+    }
+}
